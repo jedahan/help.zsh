dc425a39aadf45a8070f1c5a599e6bf0f1d08be0
Jonathan Dahan <hi@jonathan.is>
2019-07-07 17:39:03 -0400

    initial help command

---
+++ help.zsh
@@ -0,0 +1,18 @@
+# help - simple tldr wrapper that falls back to man
+
+(( $+functions[help] )) && return
+
+(( $+commands[tldr] )) && function help {
+  if [[ $# = 0 ]]; then command cat <<<'help v0.1.0
+
+help [--os <type>] <command|all>
+
+  <type>    linux, osx, sunos, other
+     all    List all commands
+
+Try `help --os linux tar`'; return; fi
+  echo $* | grep -q all && tldr --list && return
+  tldr -q $* || { tldr -q -o linux $* || command man $* }
+}
+
+(( $+functions[h] )) || alias h=help
